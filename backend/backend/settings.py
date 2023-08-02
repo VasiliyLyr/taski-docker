cac250f68d49823866c4db672a44370d035ec2bf
Василий Лапко <v.lyr@yandex.ru>
2023-08-02 21:00:05 +0500

    Fix after review

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static_backend/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
